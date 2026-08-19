f97341529146213f05d6877404ef66e35a217260
Syed-Shail <syedshailsuheb@gmail.com>
2026-08-19 20:30:49 +0530

    Add: longest-palindromic-substring - submission-1

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    string expand (string s , int l , int r ){
+        while(l>=0 && r<s.size() && s[l]==s[r]){
+            l--;
+            r++;
+        }
+        return s.substr(l+1,r-l-1);
+    }
+
+    string longestPalindrome(string s) {
+        string ans = "";
+
+        for(int i=0 ; i<s.size() ; i++){
+            string odd = expand(s,i,i+1);
+            string even = expand(s,i,i);
+            if(odd.length()>ans.length())ans=odd;
+            if(even.length()>ans.length())ans=even;
+
+        }
+        return ans;
+    }
+};
