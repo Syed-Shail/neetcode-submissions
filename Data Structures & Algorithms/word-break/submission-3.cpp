9575bcd375359f280d2e8833ab146f07770a4669
Syed-Shail <syedshailsuheb@gmail.com>
2026-09-06 20:16:33 +0530

    Add: word-break - submission-3

---
+++ Data Structures & Algorithms/word-break/submission-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> s1 (wordDict.begin(),wordDict.end());
+        int n = s.size();
+        vector<bool> dp(n+1,false);
+        dp[0]=true;
+        for(int i=1 ; i<=n ; i++){
+            for(int j=0 ; j<i ; j++){
+                if(dp[j]&& s1.count(s.substr(j,i-j))){dp[i]=true;break;}
+            }
+        }
+        return dp[n];
+    }
+};
